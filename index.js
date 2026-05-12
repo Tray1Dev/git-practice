11bd32830db7fce1dad937bec1929c50bdc242e5
Owen <owenlopez3232@gmail.com>
2026-05-12 14:51:13 -0500

    still rememebering IF cond

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const cartas = ["sky striker","mistugiri", "vanquish k9"]
+const cartas = ["sky striker","mistugiri", "vanquish k9"];
 
 for (let i = 0; i < cartas.length; i++) {
     console.log(cartas[i]);
 }
+
+let wins = 10;
+
+if (wins > 5) {
+  console.log("Buen winrate");
+}
+
+
