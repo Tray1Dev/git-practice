a673f509c804c3c27ee15c88b61e6243d6042098
Owen <owenlopez3232@gmail.com>
2026-05-20 16:00:38 -0500

    meti de nuevo python para mejorar la compresion logica

---
+++ repaso_logica.py
@@ -0,0 +1,13 @@
+numero = 1
+
+print(numero + numero)
+
+while numero < 2:
+    print("hola amigos") #esto lo esta haciendo siempre y no va a parar por que es menor que 2 siempre lo va ahcer
+    
+    break 
+listadecartas = ["sky striker raye","mitsurugi","fuwalos","infernoble"]
+
+if listadecartas != int:
+    print(listadecartas[2])
+    
