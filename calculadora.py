4ab378edf7cd6f76fd482fd82a64cf08c6bed813
Owen <owenlopez3232@gmail.com>
2026-06-09 15:39:22 -0500

    hoy mejore la calculadora sin uso de ia ni internet, cambie de false a break, tambien mejore las variables y logre poner un historial a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,49 @@
 #hacer calculadora asi que tenego que tener en cuenta que tipo de variables tengo que establecer.
 
+historial = []
+
+end ="si"
 
 while True:
-    numero = int(input("calculadora \n \n inserta un numero: "))
+    menu = input("bienvenido a la calculadora \n \n salir,si/no:").lower() 
+    if menu == end:
+        break
+    elif menu != end:
+        numero = int(input("calculadora \n \n inserta un numero: "))
         
-    numero2 = int(input("inserte otro numero:"))
+        numero2 = int(input("inserte otro numero:"))
 
-    operacion = input("elige la operacion:\n\n(+=suma -=resta *=multiplicacion /=division)")
+        operacion = input("elige la operacion:\n\n(+=suma -=resta *=multiplicacion /=division)")
 
-    if operacion == "+":
-        resultado =(numero + numero2)
-        print (resultado)
-    elif operacion == "-":
-        resultado2 =(numero - numero2)
-        print(resultado2)
-    elif operacion == "*":
-        resultado3 = (numero * numero2)
-        print (resultado3)
-    elif operacion == "/":
-        resultado4 = (numero * numero2)
-        print (resultado4)
+        if operacion == "+":
+            resultado =(numero + numero2)
+            historial.append(resultado)
+            print(resultado,historial)
+        elif operacion == "-":
+            resultado =(numero - numero2)
+            historial.append(resultado)
+            print(resultado,historial)
+        elif operacion == "*":
+            resultado = (numero * numero2)
+            historial.append(resultado)
+            print (resultado,historial)
+        elif operacion == "/":
+            resultado = (numero / numero2)
+            historial.append(resultado)
+            print (resultado,historial)
+        else:
+            print("inserta caracteres validos")
+    
     else:
-        print("inserta caracteres validos, gracias")    
-    #bucle extra para ver los ultimos resultados, creo que tengo que hacerla con alguna lista o diccionario<p>
-    bucle1 = input("quieres seguir,salir del menu o ver los ultimos resultados\n\n1 para salir\n2 para seguir\n3 para ver ultimos resultados")
+        print("elige un caracter valido")
 
-    if bucle1 == 1:
-        False
-    elif bucle1 == 3:
-        print(resultado,resultado2,resultado3,resultado4)
-    else:
-        pass 
+    
+    
+    
+    
+    
+    
+
+
 
     
---
+++ prueba_listas.py
@@ -0,0 +1,6 @@
+lista = []
+
+numero = int(input("insterte un numero para guardarlo"))
+
+lista.append(numero)
+print(lista)
