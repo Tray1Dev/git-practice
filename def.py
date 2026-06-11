deac643dc6cbfbd13394438e6659528b8d809ee2
Owen <owenlopez3232@gmail.com>
2026-06-11 12:19:21 -0500

    aniandi que la calculadora fuera una funcion asi para que pueda ampliar los menus y mas codigo. falta probar excepciones e intentos.

--- def.py
+++ def.py
@@ -1,51 +1,78 @@
-#definir funciones y entenderlas realmente como son y que despenian 
-    
-#entendamos que una funcion compone todo el codigo de el lenguaje escrito adentro de si, es decir, cualquier secunecia cualquer variable puede estar adentro de una funcion.
-
-#creare una funcion que apartir de ciertos nombres que se indique en la consola, los muestre en el mismo orden de mencion en una lista
-#datos = []
-#def ponerDatos (datos):
-#    mensaje = input("hola que datos quieres listar:")
- #   datos.append(mensaje)
-#    print(datos)
- #   return mensaje #la funcion ya queda creada, no se usa hasta que sea llamada, abajo se combina con un bucle .
-#def borrarDatos (datos):
- #   mensaje1 = input("quieres borrar los datos?:")
-#    datos.clear
-#    print (datos)
-##    return mensaje1
-#while True:
-##    ponerDatos(datos)
-##    borrarDatos(datos)
-    
-    
-##hoy voy a hacer mas practicas de funciones para meterlas en una calculadora 
 
 
-def suma ():
-    numero1 = int(input("insterte un numero:"))
-    numero2 = int(input("insterte otro numero:"))
-    resultado = numero1 + numero2
-    print(resultado)
 
-suma()
-
-def resta ():
-    numero1 = int(input("insterte un numero:"))
-    numero2 = int(input("insterte un numero:"))
-    resultado = numero1 - numero2
-    print(resultado)
-
-resta ()
-
-numero = int(input("\nInserta un numero: "))
-        numero2 = int(input("Inserte otro numero: "))
-        
-
-    
-historial.append(resultado)
+def calculadora ():
+    historial = []
+    end = "si"
+    run = "no"
+    def suma ():
+        numero = int(input("insterte un numero:"))
+        numero2 = int(input("insterte otro numero:"))
+        resultado = numero + numero2
+        print(resultado)
+        historial.append(resultado)
         print(f"Resultado: {resultado} | Historial: {historial}\n")
 
+        ## suma()
+    def resta ():
+        numero = int(input("insterte un numero:"))
+        numero2 = int(input("insterte un numero:"))
+        resultado = numero - numero2
+        print(resultado)
+        historial.append(resultado)
+        print(f"Resultado: {resultado} | Historial: {historial}\n")
+
+    ## resta ()
+    def multiplicacion ():
+        numero = int(input("insterte un numero:"))
+        numero2 = int(input("insterte un numero:"))
+        resultado = numero * numero2
+        print(resultado)
+        historial.append(resultado)
+        print(f"Resultado: {resultado} | Historial: {historial}\n")
+
+## multiplicacion ()
+    def division ():
+        numero = int(input("insterte un numero:"))
+        numero2 = int(input("insterte un numero:"))
+        resultado = numero / numero2
+        print(resultado)
+        historial.append(resultado)
+        print(f"Resultado: {resultado} | Historial: {historial}\n")
+
+## division ()
+    while True:
+        menu = input("Bienvenido a la calculadora \n \n¿Salir? (si/no): ").lower() 
+    
+        if menu == end:
+            print("Gracias por usar la calculadora. Historial final:", historial)
+            break
+        
+        elif menu == run:  
+        
+            operacion = input("Elige la operacion (+, -, *, /): ")
+
+            if operacion == "+":
+                suma()
+            elif operacion == "-":
+                resta()
+            elif operacion == "*":
+             multiplicacion()
+            elif operacion == "/":
+                division()
+            else:
+                print("Operación no válida.")
+                continue # Salta al inicio del while sin guardar nada       
+    else: 
+        print("Por favor, introduce una opción válida ('si' o 'no').\n")
+
+
+var1 = input("correr calculadora? si?")
+
+if var1 == "si":
+    calculadora()
+else:
+    print("pronto mas aplicaciones.....")
 
 
 
